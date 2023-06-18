abce37da52c1bd5d1a75c6d9298bfda004ebe8bf
SCBalate <saurabhbalate111@gmail.com>
2023-06-18 21:14:29 +0530

    commit code

--- second/src/App.js
+++ second/src/App.js
@@ -1,25 +1,119 @@
-import logo from './logo.svg';
 import './App.css';
+import HabitList from './Components/HabitList';
+import { useState } from 'react';
+import AddHabitForm from './Components/HabitForm';
+import ArchivedHabits from './Components/ArchivedHabits';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 function App() {
+
+  const [habits, setHabits] = useState([
+    {
+      "id": 1,
+      "name": "Exercise",
+      "goal": "Stay fit and healthy",
+      "frequency": "3 times per week",
+      "startDate": "2023-06-01",
+      "endDate": "2023-06-30"
+    },
+    {
+      "id": 2,
+      "name": "Read",
+      "goal": "Expand knowledge",
+      "frequency": "Every day",
+      "startDate": "2023-06-01",
+      "endDate": "2023-06-30"
+    },
+    {
+      "id": 3,
+      "name": "Meditate",
+      "goal": "Relieve stress",
+      "frequency": "Once per day",
+      "startDate": "2023-06-01",
+      "endDate": "2023-06-30"
+    }
+  ])
+  const [archivedHabits, setArchivedHabits] = useState([]);
+
+
+  const handleArchiveHabit = (habit) => {
+    setHabits((prevHabits) => {
+      const updatedHabits = prevHabits.map((h) => {
+        if (h.id === habit.id) {
+          return { ...h, archived: true };
+        }
+        return h;
+      });
+      const archived = updatedHabits.filter((h) => h.archived);
+      setArchivedHabits(archived);
+      return updatedHabits;
+    });
+  };
+
+
+  const handleEditHabit = (updatedHabit) => {
+    setHabits((prevHabits) => {
+      return prevHabits.map((habit) => {
+        if (habit.id === updatedHabit.id) {
+          return {
+            ...habit,
+            name: updatedHabit.name,
+            goal: updatedHabit.goal,
+            frequency: updatedHabit.frequency,
+            startDate: updatedHabit.startDate,
+            endDate: updatedHabit.endDate,
+          };
+        }
+        return habit;
+      });
+    });
+  };
+
+  const handleDeleteHabit = (habit) => {
+    setHabits((prevHabits) => {
+      return prevHabits.filter((h) => h.id !== habit.id);
+    });
+  };
+
+  const handleAddHabit = (newHabit) => {
+    debugger
+    const habitWithId = {
+      ...newHabit,
+      id: Math.random().toString(), 
+      name:`${newHabit?.name}`
+    };
+    setHabits((prevHabits) => [...prevHabits, habitWithId]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <HabitList habits={habits} onEditHabit={handleEditHabit} onDeleteHabit={handleDeleteHabit} onArchieveHabit={handleArchiveHabit}/>
+     <AddHabitForm onAddHabit={handleAddHabit}/>
+       <ArchivedHabits onArchievehabits={archivedHabits} />
     </div>
-  );
+  )
+  //   return (
+     
+  //     <div className="App">
+  //        <Router>
+  //       <nav>
+  //         <ul>
+  //           <li>
+  //             <Link to="/">Habit List</Link>
+  //           </li>
+  //           <li>
+  //             <Link to="/add">Add Habit</Link>
+  //           </li>
+  //         </ul>
+  //       </nav>
+  //       <Routes>
+  //         <Route path="/" element={<HabitList habits={habits} onEditHabit={handleEditHabit} onDeleteHabit={handleDeleteHabit} />} />
+  //         <Route path="/add" element={<AddHabitForm onAddHabit={handleAddHabit} />} />
+  //       </Routes>
+  //       </Router>
+  //     </div>
+   
+  // );
 }
 
 export default App;
